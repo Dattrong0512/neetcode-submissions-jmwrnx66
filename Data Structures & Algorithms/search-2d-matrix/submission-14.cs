be87f6244dd4c7199331a815e0e6ebaea8ddcfec
Lê Viết Đạt Trọng <111135514+Dattrong0512@users.noreply.github.com>
2026-03-30 10:49:22 +0700

    Add: search-2d-matrix - submission-14

---
+++ Data Structures & Algorithms/search-2d-matrix/submission-14.cs
@@ -0,0 +1,15 @@
+public class Solution {
+    public bool SearchMatrix(int[][] matrix, int target)
+    {
+        int left = 0;
+        int right = matrix[0].Length - 1;
+        while(left < matrix.Length && right >= 0)
+        {
+            int currentValue = matrix[left][right];
+            if(target == currentValue) return true;
+            else if(target > currentValue) left++;
+            else right--;
+        }
+        return false;
+    }   
+}
