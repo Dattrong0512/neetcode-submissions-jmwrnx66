85348a09724440de773a4f9bc836af13b9656cf2
Lê Viết Đạt Trọng <111135514+Dattrong0512@users.noreply.github.com>
2026-04-10 15:25:58 +0700

    Add: longest-repeating-substring-with-replacement - submission-2

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-2.cs
@@ -0,0 +1,22 @@
+public class Solution
+{
+    public int CharacterReplacement(string s, int k)
+    {
+        int[] storeCharacter = new int[26];
+        int maxLength = 0;
+        int maxCount = 0;
+        int left = 0;
+        for(int right = 0; right < s.Length; right++)
+        {
+            storeCharacter[s[right] - 'A']++;
+            maxCount = maxCount > storeCharacter[s[right] - 'A'] ? maxCount : storeCharacter[s[right] - 'A'];
+            while((right - left + 1) - maxCount > k)
+            {
+                storeCharacter[s[left] - 'A']--;
+                left++;
+            }
+            maxLength =  right - left + 1;
+        }
+        return maxLength;
+    }
+}
