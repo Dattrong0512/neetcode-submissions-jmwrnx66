eae206f7b8ad3feaedf6f67dd60f971c3cfcba88
Lê Viết Đạt Trọng <111135514+Dattrong0512@users.noreply.github.com>
2026-04-10 09:34:11 +0700

    Add: buy-and-sell-crypto - submission-3

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-3.cs
@@ -0,0 +1,12 @@
+public class Solution {
+    public int MaxProfit(int[] prices) {
+        int minPrice = prices[0];
+        int bestStock = 0;
+        for(int i = 1; i < prices.Length; i++)
+        {
+            minPrice = prices[i] < minPrice ? prices[i] : minPrice;
+            bestStock = (prices[i] - minPrice) > bestStock ?  (prices[i] - minPrice) : bestStock;
+        }
+        return bestStock;
+    }
+}
