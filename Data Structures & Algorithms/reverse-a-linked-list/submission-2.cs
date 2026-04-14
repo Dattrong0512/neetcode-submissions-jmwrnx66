0b8a9369f0d4cbce1cf63aa2f5ccb1c19885a2ff
Lê Viết Đạt Trọng <111135514+Dattrong0512@users.noreply.github.com>
2026-04-14 15:19:58 +0700

    Add: reverse-a-linked-list - submission-2

---
+++ Data Structures & Algorithms/reverse-a-linked-list/submission-2.cs
@@ -0,0 +1,26 @@
+
+// public class ListNode {
+//     public int val;
+//     public ListNode next;
+//     public ListNode(int val=0, ListNode next=null) {
+//         this.val = val;
+//         this.next = next;
+//     }
+// }
+ 
+public class Solution {
+    public ListNode ReverseList(ListNode head) {
+        ListNode current = head;
+        ListNode prev = null;
+        if(head == null || head.next == null)
+            return head;
+        while(current != null)
+        {
+            ListNode temp = current.next;
+            current.next = prev;
+            prev = current;
+            current = temp;
+        }
+        return prev;
+    }
+}
