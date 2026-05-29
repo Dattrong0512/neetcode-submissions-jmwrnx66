e6886c02141aefc6c5c816ca5592d595a756f55b
Lê Viết Đạt Trọng <111135514+Dattrong0512@users.noreply.github.com>
2026-05-29 14:45:09 +0700

    Add: subsets - submission-2

---
+++ Data Structures & Algorithms/subsets/submission-2.cs
@@ -0,0 +1,28 @@
+public class Solution {
+    public List<List<int>> _listReturn;
+    public List<int> _current;
+    public Solution()
+    {
+        _listReturn = new List<List<int>>();
+        _current = new List<int>();
+    }
+
+    public List<List<int>> Subsets(int[] nums)
+    {
+        BackTracking(nums, 0);
+        return _listReturn;
+    } 
+
+    public void BackTracking(int[] nums, int index)
+    {
+        if(nums.Length == index)
+        {
+            _listReturn.Add(new List<int>(_current));
+            return;
+        }
+        BackTracking(nums, index + 1);
+        _current.Add(nums[index]);
+        BackTracking(nums, index + 1);
+        _current.RemoveAt(_current.Count - 1);
+    }
+}
