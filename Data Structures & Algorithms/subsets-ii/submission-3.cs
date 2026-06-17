6080a134e6a80569c66260bb52a0285f4033c83c
Lê Viết Đạt Trọng <111135514+Dattrong0512@users.noreply.github.com>
2026-06-17 14:26:27 +0700

    Add: subsets-ii - submission-3

---
+++ Data Structures & Algorithms/subsets-ii/submission-3.cs
@@ -0,0 +1,28 @@
+public class Solution {
+    public List<List<int>> _listReturn = new List<List<int>>();
+    public List<int> _current = new List<int>();
+
+    public List<List<int>> SubsetsWithDup(int[] nums)
+    {
+        Array.Sort(nums);
+        BackTracking(nums, 0);
+        return _listReturn;
+    }
+
+    public void BackTracking(int[] nums, int index)
+    {
+        if(index >= nums.Length)
+        {
+            _listReturn.Add(new List<int>(_current));
+            return;
+        }
+        _current.Add(nums[index]);
+        BackTracking(nums, index + 1);
+        _current.RemoveAt(_current.Count - 1);
+        while(index + 1 < nums.Length && nums[index] == nums[index + 1])
+        {
+            index++;
+        }
+        BackTracking(nums, index + 1);
+    }
+}
